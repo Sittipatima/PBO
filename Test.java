9bac4f86b499a5b1e471c31ab9fe7f9c5e02a5f0
Sittipatima <sittipatima958@gmail.com>
2025-03-06 20:33:35 +0800

    Test.java

---
+++ Test.java
@@ -0,0 +1,9 @@
+import javax.xml.crypto.Data;
+public class Test {
+    public static void main(String[] args) {
+        database db = new database();
+        db.open();
+        db.view();
+
+    }
+}
